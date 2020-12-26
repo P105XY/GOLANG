afd9d67d8d5b09fd58dc4b793d55c00ad40333db
P105XY <p105xy85@gmail.com>
2020-12-26 23:45:44 +0900

    append URL checker

--- src/main.go
+++ src/main.go
@@ -1,22 +1,43 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-
-	"./MyDict"
+	"net/http"
 )
 
-func main() {
-	mdic := MyDict.Dictionary{}
+var errRequestFailed = errors.New("Request Failed")
 
-	baseWord := "Hello"
-	mdic.Add(baseWord, "first")
-	mdic.Search(baseWord)
-	mdic.Delete(baseWord)
-	word, err := mdic.Search(baseWord)
-	if err != nil {
-		fmt.Println(err)
+func main() {
+	//REMEMBER : map 생성 시, 선언과 동시에 초기화 필수.
+	//어떤 값이든 초기화를 시켜야, 나중에 따로 대입이나 재정의가 가능하다.
+	var results = make(map[string]string)
+
+	urls := []string{
+		"https://www.google.com",
+		"https://www.naver.com",
 	}
 
-	fmt.Println(word)
+	for _, url := range urls {
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+	for url, result := range results {
+		fmt.Println(url, result)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("Checking : ", url)
+	resp, err := http.Get(url)
+
+	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err)
+		return errRequestFailed
+	}
+	return nil
 }
